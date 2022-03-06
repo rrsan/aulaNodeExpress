b0f0fdbe9580e02ee8beb072e7eb6769b56eb9b4
rrsan <rodrocha.santos@gmail.com>
2022-03-06 12:12:01 -0300

    atualização para recebimento json

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-const bodyParser = require('body-parser')
-const express = require("express")
+const bodyParser = require('body-parser');
+const express = require("express");
 
-const jsonParser = bodyParser.json()
+const jsonParser = bodyParser.json();
 
-const server = express()
+const server = express();
 
-const usuarios = [{id:1, nome:"Rodrigo",sobrenome:"Santos"},{id:2, nome:"Aline",sobrenome:"Rocha"}]
+server.use(express.json());
+
+const usuarios = [{id:1, nome:"Rodrigo",sobrenome:"Santos"},{id:2, nome:"Aline",sobrenome:"Rocha"}];
 
 server.get( "/" , function (req, res) {
     res.send(usuarios)
-})
+});
 
 server.get( "/:id" , function (req, res) {
     const {id} = req.params
     const aux = usuarios.find((usuario) => usuario.id == id)
     res.send(aux)
-})
+});
 
 server.post("/",jsonParser,(req,res) => {
     console.log(req.body)
     const aux = req.body
     usuarios.push(aux)
     res.send(usuarios)
-})
+});
 
 server.delete("/:index",(req,res) => {
     const {index} = req.params;
     usuarios.splice(index,1)
     res.send(usuarios)
-})
+});
 
-server.listen(3000)
+server.listen(3000);
 
